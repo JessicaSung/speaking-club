8c44f404f5ac3ca1d50fb4cecf3c6f3166c7c879
JessicaSung <18690072+JessicaSung@users.noreply.github.com>
2019-05-07 19:27:48 -0500

    sort feature added to table head ONLY

--- src/components/MemberTable.js
+++ src/components/MemberTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -6,70 +6,153 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Tooltip from '@material-ui/core/Tooltip';
 import Paper from '@material-ui/core/Paper';
 import MemberRow from './MemberRow';
+
+const desc = (a, b, orderBy) => {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+};
+
+const getSorting = (order, orderBy) => {
+  return order === 'desc'
+    ? (a, b) => desc(a, b, orderBy)
+    : (a, b) => -desc(a, b, orderBy);
+};
+
+const columnNames = [
+  { id: 'name', label: 'Name' },
+  { id: 'path', label: 'Path' },
+  { id: 'email', label: 'Email' },
+  { id: 'phone', label: 'Phone' }
+];
+
+class MemberTableHead extends Component {
+  createSortHandler = property => event => {
+    this.props.onRequestSort(event, property);
+  };
+
+  render() {
+    const { order, orderBy } = this.props;
+
+    return (
+      <TableHead>
+        <TableRow>
+          {columnNames.map(column => {
+            return (
+              <TableCell
+                key={column.id}
+                sortDirection={orderBy === column.id ? order : false}
+              >
+                <Tooltip title="Sort" placement={'bottom-end'} enterDelay={300}>
+                  <TableSortLabel
+                    active={orderBy === column.id}
+                    direction={order}
+                    onClick={this.createSortHandler(column.id)}
+                  >
+                    {column.label}
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+            );
+          }, this)}
+        </TableRow>
+      </TableHead>
+    );
+  }
+}
+
+MemberTableHead.propTypes = {
+  onRequestSort: PropTypes.func.isRequired,
+  order: PropTypes.string.isRequired,
+  orderBy: PropTypes.string.isRequired
+};
 
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
-    overflowX: 'auto'
+    marginTop: theme.spacing.unit * 3
   },
   table: {
-    minWidth: 700
+    minWidth: 1020
+  },
+  tableWrapper: {
+    overflowX: 'auto'
   }
 });
 
-const MemberTable = props => {
-  const { classes } = props;
+class MemberTable extends Component {
+  state = {
+    order: 'asc',
+    orderBy: 'calories'
+  };
 
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell>Path</TableCell>
-            <TableCell>Email</TableCell>
-            <TableCell>Phone</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-          <MemberRow />
-        </TableBody>
-      </Table>
-    </Paper>
-  );
-};
+  handleRequestSort = (event, property) => {
+    const orderBy = property;
+    let order = 'desc';
+
+    if (this.state.orderBy === property && this.state.order === 'desc') {
+      order = 'asc';
+    }
+
+    this.setState({ order, orderBy });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { order, orderBy } = this.state;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <MemberTableHead
+            order={order}
+            orderBy={orderBy}
+            onRequestSort={this.handleRequestSort}
+          />
+          <TableBody>
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+            <MemberRow />
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
+}
 
 MemberTable.propTypes = {
   classes: PropTypes.object.isRequired
